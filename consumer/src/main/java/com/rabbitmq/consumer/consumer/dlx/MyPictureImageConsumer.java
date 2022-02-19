936aada94f3c05e9d3608baa40a82f0eafe354d9
kimdh-hi <zbeld123@gmail.com>
2022-02-19 17:33:10 +0900

    DLX - channel (basicReject, basicAck) 예외발생 시 무한 requeue 해결

--- consumer/src/main/java/com/rabbitmq/consumer/consumer/dlx/MyPictureImageConsumer.java
+++ consumer/src/main/java/com/rabbitmq/consumer/consumer/dlx/MyPictureImageConsumer.java
@@ -2,11 +2,14 @@ package com.rabbitmq.consumer.consumer.dlx;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.consumer.entity.Picture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -22,16 +25,18 @@ public class MyPictureImageConsumer {
     }
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void listener(String message) throws IOException {
+    public void listener(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         Picture picture = mapper.readValue(message, Picture.class);
 
         // 일반적인 queue에서 메시지에 대한 처리 중 예외가 발생했다면 다시 Queue에 넣고 재실행하는 과정이 무한하게 반복된다.
         if (picture.getSize() >= 9000) {
+            channel.basicReject(tag, false);
+            return;
             // q.mypicture.image에 dead letter exchange(DLX)를 설정하고 AmqpRejectAndDontRequeueException를 발생시키면
             // 해당 메시지는 무한하게 requeue 되지 않고 dlx로 보내진다.
-            throw new AmqpRejectAndDontRequeueException("too large size image. picture=" + picture);
+//            throw new AmqpRejectAndDontRequeueException("too large size image. picture=" + picture);
         }
-
         logger.info("[image] message = {}", picture);
+        channel.basicAck(tag, false);
     }
 }
