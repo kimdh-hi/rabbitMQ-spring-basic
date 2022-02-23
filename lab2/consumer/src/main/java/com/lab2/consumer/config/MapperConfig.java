01f5f62cd69c28c46c84d8e706713aae8e6a2ed7
kimdh-hi <zbeld123@gmail.com>
2022-02-23 15:29:19 +0900

    lab2 init

---
+++ lab2/consumer/src/main/java/com/lab2/consumer/config/MapperConfig.java
@@ -0,0 +1,21 @@
+package com.lab2.consumer.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder().findAndAddModules().build();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
